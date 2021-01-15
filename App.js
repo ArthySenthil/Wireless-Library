ff96579c00c30d021b3937a8f3da3c3dd35496a4
ArthySenthil <arthyshankari@gmail.com>
2021-01-16 04:45:39 +0530

    seek camera permission

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import TransactionScreen from './screens/BookTransactionScreen.js'
+import SearchScreen from './screens/SearchScreen.js'
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <AppContainer/>
+     );
+  }
+  
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +24,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
+
+});
+
+const AppContainer = createAppContainer(TabNavigator);
